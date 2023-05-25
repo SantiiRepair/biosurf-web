563a7ca5807358eb9a2d1ce51770781975f33fc6
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-05-25 14:58:27 +0000

    Update 4 files

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -9,45 +9,67 @@ import {
   Link,
   Button,
   Heading,
-  Text,Center,
+  Text,
+  Center,
   useColorModeValue,
-} from '@chakra-ui/react';
-import { FaFacebook } from 'react-icons/fa';
-              
-  function FacebookButton() {
+} from "@chakra-ui/react";
+import { FaFacebook, FaGoogle } from "react-icons/fa";
+
+function FacebookButton() {
   return (
     <Center p={8}>
       <Button
-        w={'full'}
-        maxW={'md'}
-        colorScheme={'facebook'}
-        leftIcon={<FaFacebook />}>
+        w={"full"}
+        maxW={"md"}
+        colorScheme={"facebook"}
+        leftIcon={<FaFacebook />}
+      >
         <Center>
           <Text>Continue with Facebook</Text>
         </Center>
       </Button>
     </Center>
   );
-}            
+}
+
+function GoogleButton() {
+  return (
+    <Center p={8}>
+      <Button
+        w={"full"}
+        maxW={"md"}
+        variant={"outline"}
+        leftIcon={<FaGoogle />}
+      >
+        <Center>
+          <Text>Sign in with Google</Text>
+        </Center>
+      </Button>
+    </Center>
+  );
+}
+
 export default function SimpleCard() {
   return (
     <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            to enjoy all of our cool <Link color={"blue.400"}>features</Link> ✌️
           </Text>
         </Stack>
         <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
           <Stack spacing={4}>
             <FormControl id="email">
               <FormLabel>Email address</FormLabel>
@@ -59,21 +81,24 @@ export default function SimpleCard() {
             </FormControl>
             <Stack spacing={10}>
               <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              >
                 <Checkbox>Remember me</Checkbox>
-                <Link color={'blue.400'}>Forgot password?</Link>
+                <Link color={"blue.400"}>Forgot password?</Link>
               </Stack>
               <Button
-                bg={'blue.400'}
-                color={'white'}
+                bg={"blue.400"}
+                color={"white"}
                 _hover={{
-                  bg: 'blue.500',
-                }}>
+                  bg: "blue.500",
+                }}
+              >
                 Sign in
               </Button>
-<FacebookButton/>
+              <FacebookButton />
+              <GoogleButton />
             </Stack>
           </Stack>
         </Box>
