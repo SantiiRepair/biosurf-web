288cb9cd870a4e9589406601791d72e644ab1392
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-05-25 15:23:05 +0000

    Update .gitignore and _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { AppProps } from "next/app";
 import { ChakraProvider, Flex } from "@chakra-ui/react";
-import { Config, DAppProvider, Mainnet } from "@usedapp/core";
+// import { Config, DAppProvider, Mainnet } from "@usedapp/core";
 import { MetaMaskInpageProvider } from "@metamask/providers";
 import theme from "../theme";
 import Header from "../modules/components/header";
@@ -13,30 +13,30 @@ declare global {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const config: Config = {
-    readOnlyChainId: 1,
-    readOnlyUrls: {
-      [Mainnet.chainId]: process.env.MAINNET_RPC! || process.env.SEPOLIA_RPC!,
-    },
-    autoConnect: false,
-  };
+  // const config: Config = {
+  // readOnlyChainId: 1,
+  // readOnlyUrls: {
+  // [Mainnet.chainId]: process.env.MAINNET_RPC! || process.env.SEPOLIA_RPC!,
+  // },
+  // autoConnect: false,
+  // };
 
-  return (  
+  return (
     <ChakraProvider resetCSS theme={theme}>
-      <DAppProvider config={config}>
-        <Head>
-          <title>Biosurf NFT</title>
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" href="/img/logo.svg" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="description" content="CAI NFT" />
-        </Head>
+      {/*<DAppProvider config={config}> */}
+      <Head>
+        <title>Biosurf NFT</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/img/logo.svg" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="description" content="CAI NFT" />
+      </Head>
 
-        <Flex direction={"column"}>
-          <Header />
-          <Component {...pageProps} />
-        </Flex>
-      </DAppProvider>
+      <Flex direction={"column"}>
+        <Header />
+        <Component {...pageProps} />
+      </Flex>
+      {/*</DAppProvider>  */}
     </ChakraProvider>
   );
 }
