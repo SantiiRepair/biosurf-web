2316f78f90f40a8cf75b0c2910653c603abba531
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-06-01 16:55:55 +0000

    Update NewComment.tsx

--- src/modules/components/comment/NewComment.tsx
+++ src/modules/components/comment/NewComment.tsx
@@ -1,86 +1,104 @@
-import React, {useState, useEffect} from 'react'
-import { Box, Button, Flex, Text, Textarea } from '@chakra-ui/react';
-import UserAvatar from './UserAvatar';
-import data from '../data.json';
-import {useComments} from '../useComments';
+import React, { useState, useEffect } from "react";
+import { Box, Button, Flex, Text, Textarea } from "@chakra-ui/react";
+import UserAvatar from "../media/UserAvatar";
+import data from "../data.json";
+import { useComments } from "../../../hooks/useComments";
+
+interface Props {
+  isUpdate: boolean;
+  replyingTo?: string;
+  isNewComment: boolean;
+  id?: number;
+  parentId?: number;
+  closeEditor: () => void;
+}
 
 const AuthUser = data.currentUser;
 
-function CommentEditor({isUpdate, replyingTo ,isNewComment, id, parentId, closeEditor}) {
+function CommentEditor({
+  isUpdate,
+  replyingTo,
+  isNewComment,
+  id,
+  parentId,
+  closeEditor,
+}: Props) {
+  const [commentText, setCommentText] = useState<string>("");
 
-  const [commentText, setCommentText] = useState('');
-  const {reply, addTopLevelComment, updateComment} = useComments();
-  
+  const { reply, addTopLevelComment, updateComment } = useComments();
 
   useEffect(() => {
-    replyingTo && setCommentText(`@${replyingTo}, `);
-  },[])
+    if (replyingTo) setCommentText(`@${replyingTo}, `);
+  }, [replyingTo]);
 
+  const removeTagFromCommentText = (): string => {
+    return commentText.substring(replyingTo!.length + 2);
+  };
 
-  useEffect(() => {
+  const handleClick = () => {
+    if (commentText === "") return;
 
-  },[commentText])
-  
-
-  // removes the @username tag from the text to be saved
-  const removeTagFromCommentText = () => {
-    return commentText.substring(replyingTo.length+2);
-  } 
-
-  const _handleClick = () => {
-    if(commentText === '') return;
-    
-    //  reply
-    if(!isUpdate && !isNewComment){
+    if (!isUpdate && !isNewComment) {
       let cleanedText = removeTagFromCommentText().trim();
-      reply(parentId, AuthUser, cleanedText, replyingTo);
-      
+      reply(parentId!, AuthUser, cleanedText, replyingTo!);
+
       closeEditor();
     }
 
-    // new Comment
-    if(isNewComment){
+    if (isNewComment) {
       addTopLevelComment(commentText, AuthUser);
     }
 
-    // update
-    if(isUpdate){
-      console.log('update clicked')
-      updateComment(id,parentId, commentText);
+    if (isUpdate) {
+      updateComment(id!, parentId!, commentText);
       closeEditor();
     }
 
     reset();
+  };
 
-  }
-
-  function reset(){
-    setCommentText('');
-  }
+  const reset = () => {
+    setCommentText("");
+  };
 
   return (
-    <Box className='comment' bg={'white'} borderRadius={'xl'} my={4} p={5} boxShadow={'sm'}>
-     <Box>
-      <Flex alignItems={'start'} justifyContent="space-between">
-        {/* User Image */}
-        <UserAvatar image={AuthUser.image.webp}/>
-        {/*New Comment Box */}
-        <Textarea resize={'none'} display={'flex'} width={'100%'} flex={1} mx={4} color={"grayish_blue"} fontWeight={'medium'}
-          onChange={(e) => setCommentText(e.target.value)}
-          value={commentText}
-        />
-      <Button bg={'moderate_blue'}
-        onClick={() => _handleClick()}
-        _hover={{
-          opacity: 0.6
-        }}
-      >
-          <Text fontWeight={'black'} color={'white'}>{isUpdate ? 'Update' : isNewComment ? 'Send' : 'Reply'}</Text>
-        </Button>
-      </Flex>
-     </Box>
+    <Box
+      className="comment"
+      bg={"white"}
+      borderRadius={"xl"}
+      my={4}
+      p={5}
+      boxShadow={"sm"}
+    >
+      <Box>
+        <Flex alignItems={"start"} justifyContent="space-between">
+          <UserAvatar image={AuthUser.image.webp} />
+          <Textarea
+            resize={"none"}
+            display={"flex"}
+            width={"100%"}
+            flex={1}
+            mx={4}
+            color={"grayish_blue"}
+            fontWeight={"medium"}
+            onChange={(e) => setCommentText(e.target.value)}
+            value={commentText}
+          />
+          <Button
+            bg={"moderate_blue"}
+            onClick={() => handleClick()}
+            _hover={{
+              opacity: 0.6,
+            }}
+          >
+            <Text fontWeight={"black"} color={"white"}>
+              {isUpdate ? "Update" : isNewComment ? "Send" : "Reply"}
+            </Text>
+          </Button>
+        </Flex>
+      </Box>
     </Box>
-  )
+  );
 }
 
-export default CommentEditor
+export default CommentEditor;
