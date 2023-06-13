038dba95ed475015c8f67aabefbbff38825d797e
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-06-13 03:27:42 +0000

    Update 4 files

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        DATABASE_URL: '',
-        GOOGLE_CLIENT_ID: '',
-        GOOGLE_CLIENT_SECRET: '',
-        NEXTAUTH_SECRET: '',
-        NEXTAUTH_URL: '',
+        BACKEND_URL: '',
     },
 };
 
